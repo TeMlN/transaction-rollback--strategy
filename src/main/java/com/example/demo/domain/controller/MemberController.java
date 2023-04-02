20b4cd0301ec056b297b78e0373f1dd1a31f1fd0
taem1n1 <youman7953@naver.com>
2023-04-02 22:31:09 +0900

    Member API End Point

--- src/main/java/com/example/demo/domain/controller/MemberController.java
+++ src/main/java/com/example/demo/domain/controller/MemberController.java
@@ -1,2 +1,21 @@
-package com.example.demo.domain.controller;public class MemberController {
+package com.example.demo.domain.controller;
+
+import com.example.demo.domain.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/member")
+    private ResponseEntity registerMember(@RequestParam String name) throws Exception {
+        memberService.save(name);
+        return ResponseEntity.ok().build();
+    }
 }
