40e5e7469b93c5a100a23cffb376341b0970db76
taem1n1 <youman7953@naver.com>
2023-04-02 22:30:55 +0900

    도메인 모델

--- src/main/java/com/example/demo/domain/Member.java
+++ src/main/java/com/example/demo/domain/Member.java
@@ -1,2 +1,17 @@
-package com.example.demo.domain;public class member {
+package com.example.demo.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity @Builder @Getter
+@AllArgsConstructor @NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
 }
